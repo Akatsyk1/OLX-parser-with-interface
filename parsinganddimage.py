b45cc607c48eb97e705a772dff140283e61d25c1
Akatsyki <37961332+Akatsyk1@users.noreply.github.com>
2022-06-30 17:19:40 +0300

    Update parsinganddimage.py

--- parsinganddimage.py
+++ parsinganddimage.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 import time
-import pickle
 from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
 import requests
